2a044eb3c01dd13327306e49df37dddce65348d7
hamayanShowa-ele <hamayan@showa-ele.jp>
2018-09-19 15:40:13 +0900

    update file.

--- peripheral.h
+++ peripheral.h
@@ -10,21 +10,10 @@
 /*************************************************************************/
 /* プロトタイプ宣言                                                      */
 /*************************************************************************/
-void Dly100us( int delay );
-void Dly50us( int delay );
-void Dly10us( int delay );
-void Dly1us( int delay );
-void WaitMs( int delay );
-void WaitUs( unsigned long delay );
-
 void GPIO_Terminal_Init( GPIO_TypeDef* GPIOx, int Mode, u16 Pins );
 
 enum  GPIO_MODE_TYPE {OUT2M_PP,OUT25M_PP,OUT50M_PP,OUT100M_PP,OUT2M_OD,OUT25M_OD,OUT50M_OD,OUT100M_OD,
   IN_FLOAT,IN_UP,IN_DOWN,AF_PP,AF_OD,AN_IN,};
-
-void gpioInit( void );
-void beginSCI( int ch, unsigned long brr );
-void terminateSCI( int ch );
 
 #endif	/* _PERIPHERAL_H_ */
 
